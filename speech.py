5856f90350b9085261bd13acab773cc814e3e30b
Varun Jha <48400968+varunjha18@users.noreply.github.com>
2021-05-26 23:07:47 +0530

    Update speech.py

--- speech.py
+++ speech.py
@@ -54,7 +54,7 @@ def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
-    server.login('varunjha1245@gmail.com', 'Varun@2000')
+    server.login('varunjha1245@gmail.com', '*********')
     server.sendmail('varunjha1245@gmail.com', to, content)
     server.close()
 
